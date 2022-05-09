51e7dbbb68271fb518046450fc3df23d6b2bff4d
JosueOb <josuericardocando@hotmail.com>
2022-05-08 20:04:18 -0500

    PSA-001 Add test users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,35 +3,33 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
+
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
+    public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'username' => $this->faker->word . "." . $this->faker->randomNumber(5),
+            'phone_number' => '09' . $this->faker->randomNumber(8),
+            'home_phone_number' => '02' . $this->faker->randomNumber(7),
+            'address' => $this->faker->streetAddress,
+            'password' => Hash::make('secret'),
             'email' => $this->faker->unique()->safeEmail(),
+            'birthdate' => $this->faker->dateTimeBetween('-70 years', '-18 years'),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
-     *
-     * @return static
      */
-    public function unverified()
+    public function unverified(): static
     {
         return $this->state(function (array $attributes) {
             return [
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,17 +8,30 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+
+            /*unique columns*/
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username', 20)->unique();
+
+            /*required columns*/
+            $table->string('first_name', 35);
+            $table->string('last_name', 35);
+            $table->string('phone_number', 10);
+            $table->string('home_phone_number', 9);
+            $table->string('address', 50);
             $table->string('password');
+            $table->boolean('state')->default(true);
+
+            /*nullable columns*/
+            $table->date('birthdate')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+
+            /*special columns*/
             $table->rememberToken();
             $table->timestamps();
         });
@@ -26,10 +39,8 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
